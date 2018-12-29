aaed6eee69a128cd51b648909bcac01a7c1d2149
Amit <amrx101@gmail.com>
2018-12-29 11:30:58 +0530

    Added reducers

---
+++ src/reducers/authedUser.js
@@ -0,0 +1,11 @@
+import { AUTHED_USER } from '../actions/authedUser'
+
+export default function authedUser (state = null, action) {
+    switch(action.type){
+        case AUTHED_USER:
+            return action.user
+        default:
+            return state
+    }
+
+}
