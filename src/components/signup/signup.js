613c980c69e517041d68ffabbb9b814f46ac871f
Amit <amrx101@gmail.com>
2018-12-30 11:39:26 +0530

    Added signup page

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -2,40 +2,58 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 
-class SignUp extends Component {
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', id: '', avatarURL: '', newRegistration: false};
 
-    state = {
-        name: '',
-        avatar: '',
-        id: '',
-        newRegistration: false,
-    }
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeId = this.handleChangeId.bind(this);
+    this.handleChangeAvatar = this.handleChangeAvatar.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render() {
-        return (
-			<div className="Modal">
-				<form 
-					onSubmit={this.props.onSubmit}
-					className="ModalForm">
-					<input
-						id="name"
-						type="text"
-						placeholder="Jack-Edward Oliver" />
-					<input
-						id="username"
-						type="email"
-						placeholder="mrjackolai@gmail.com" />
-					<input
-						id="password"
-						type="password"
-						placeholder="password" />
-					<button>
-						Signup <i className="fa fa-fw fa-chevron-right"></i>
-					</button>
-				</form>
-			</div>
-		);
-    }
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleChangeId(event) {
+      this.setState({id: event.target.value})
+  }
+
+  handleChangeAvatar(event) {
+      this.setState({avatarURL: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.name);
+    event.preventDefault();
+    // TODO: Dispatch authedUser
+    // Set this.state.newRegistration to True
+    this.setState({newRegistration: true})
+  }
+
+  render() {
+      const {newRegistration} = this.state
+      if (newRegistration === true){
+          console.log("There is a user. We need to switch to user view")
+          return (
+            <h3> We have a USER, redirect to home page </h3>
+          ) 
+      }
+      return (
+          <div className="Modal">
+              <div className="input_div">
+                <form onSubmit={this.handleSubmit} className="ModalForm">
+                    <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+                    <input type="text" value={this.state.id} onChange={this.handleChangeId} />
+                    <input type="text" value={this.state.avatarURL} onChange={this.handleChangeAvatar}/>
+                    <input type="submit" value="Submit" />
+                </form>
+              </div>
+          </div>
+    );
+  }
 }
 
 export default connect()(SignUp)
