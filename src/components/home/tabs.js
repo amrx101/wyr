c57c2bbe2527e9cea0c4d81192c1f0122ac3c3e6
Amit <amrx101@gmail.com>
2018-12-31 14:47:25 +0530

    different components

--- src/components/home/tabs.js
+++ src/components/home/tabs.js
@@ -5,40 +5,33 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 
 class ControlledTabs extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleSelect = this.handleSelect.bind(this);
-
-    this.state = {
-      key: 1
-    };
-  }
-
-  handleSelect(key) {
-    alert(`selected ${key}`);
-    console.log("SELECTED IS", this.state.key);
-    this.setState({ key });
-  }
 
   render() {
     return (
-        
-       <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+    <Tabs>
+        <TabList>
+            <Tab><h1>Unanswered</h1></Tab>
+            <Tab><h1> Answered</h1></Tab>
+        </TabList>
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
+        <TabPanel> <Home/> </TabPanel>
+        <TabPanel> <Home1/> </TabPanel>
+    </Tabs>
     );
   }
 }
+
+class Home extends React.Component {
+    render() {
+        return <h2> HELLO THERE </h2>
+    }
+}
+
+class Home1 extends React.Component {
+    render() {
+        return <h2> GENERAL KENOBI </h2>
+    }
+}
+
 
 export default connect()(ControlledTabs)
